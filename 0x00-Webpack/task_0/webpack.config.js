58a7a258394446a48720d2e7d058b6df1c571ce0
Aaron Charles <113832025+aceAaronCharles@users.noreply.github.com>
2023-08-24 11:45:09 +0300

    Webpack using a basic installation

--- 0x00-Webpack/task_0/webpack.config.js
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'task0.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
 };
